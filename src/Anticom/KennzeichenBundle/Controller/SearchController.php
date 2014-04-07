a23b7c49cbcb1bf5b9f4c327dedbe3db5f1e20c4
muehlbti <anticom@hotmail.de>
2014-04-07 11:16:27 +0200

    Added views
    Also implemented wiki bridge

---
+++ src/Anticom/KennzeichenBundle/Controller/SearchController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muehlbti
+ * Date: 07.04.14
+ * Time: 09:24
+ */
+
+namespace Anticom\KennzeichenBundle\Controller;
+
+
+class SearchController {
+
+} 
