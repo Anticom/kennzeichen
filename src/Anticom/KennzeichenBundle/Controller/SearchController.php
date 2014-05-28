12824506ee7740689c756ef0cc69851f5525b756
Anticom <anticom@hotmail.de>
2014-05-28 13:05:23 +0200

    minor syntax fix

--- src/Anticom/KennzeichenBundle/Controller/SearchController.php
+++ src/Anticom/KennzeichenBundle/Controller/SearchController.php
@@ -24,7 +24,7 @@ class SearchController extends Controller
         return $this->render(
             'AnticomKennzeichenBundle:Search:search.html.twig',
             array(
-                'kennzeichen' = $kennzeichen
+                'kennzeichen' => $kennzeichen
             )
         );
     }
